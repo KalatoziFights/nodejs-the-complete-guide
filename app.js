b43f4bcd748a26c68a4987a5a4384fd30b411423
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2022-12-11 23:06:26 +0400

    redirecting requests

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const http = require("http");
+const fs = require("fs");
+// file system
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
 
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>home</title></head>");
-    res.write("<body><h1>home page is very good.</h1></body>");
+    res.write(`<body><form action="/message" method="POST">
+    <input type="text" name="message"/></form></body>`);
+    res.write("<button>send</button>");
     res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
