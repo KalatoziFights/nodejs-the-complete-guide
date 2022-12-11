d46aaf82011b00097a590d28ddcb3646432f7261
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2022-12-11 22:47:34 +0400

    routing requests

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers);
+  const url = req.url;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>home</title></head>");
+    res.write("<body><h1>home page is very good.</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>fighter</title></head>");
+  res.write("<body>fight</body>");
+  res.write("</html>");
+  res.end();
 });
 
 // listen incoming requests
