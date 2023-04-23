f0ca1ac77e77a72a3925c134483e150fa2a670e5
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-23 19:25:04 +0400

    assignment 1

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const http = require("http");
 
-const routes = require("./routes");
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
 
-console.log(routes);
+  if (url === "/") {
+    res.write("<html>");
+    res.write("This is a home page.");
+    res.write(
+      "<form method='post' action='/create-user'><input type='text' name='username'/><button type='submit'>submit</button></form>"
+    );
+    res.write("</html>");
+    res.end();
+  }
 
-const server = http.createServer(routes);
+  if (url === "/users") {
+    res.write("<html>");
+    res.write("<ul><li>user 1</li><li>user 2</li></ul>");
+    res.write("</html>");
+    res.end();
+  }
 
-server.listen(3000);
+  if (url === "/create-user" && method === "POST") {
+    req.on("data", (chunk) => {
+      console.log(chunk.toString().split("="));
+      res.write("<html>");
+      res.write(`Welcome ${chunk.toString().split("=")[1]}`);
+      res.write("</html>");
+      res.end();
+    });
+  }
+});
+
+server.listen(3000, () => console.log("server is running"));
