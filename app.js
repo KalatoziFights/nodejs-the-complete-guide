c7c2856f4d65d0ff373bb5180c683a37f8218f7e
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-25 02:44:32 +0400

    parsing incoming requests

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/users", (req, res, next) => {
-  console.log("users middleware");
-  res.send("<h1>Users Page</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("main middleware");
   res.send("<h1>hello from main express</h1>");
 });
 
