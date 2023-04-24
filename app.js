c6aa6e399efdf2446b70a7e3cc20008254c72fbf
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-25 01:31:19 +0400

    handeling different routes

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+app.use("/add-product", (req, res, next) => {
+  console.log("another middleware");
+  res.send("<h1>Add Product Page</h1>");
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("another middleware");
   res.send("<h1>hello from express</h1>");
 });
