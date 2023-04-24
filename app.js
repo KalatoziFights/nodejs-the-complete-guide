eb4830c91b738e7b0cdca1687b07d95420f41ea6
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-25 01:54:54 +0400

    assignment 2

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
-  console.log("another middleware");
-  res.send("<h1>Add Product Page</h1>");
+app.use("/users", (req, res, next) => {
+  console.log("users middleware");
+  res.send("<h1>Users Page</h1>");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("another middleware");
-  res.send("<h1>hello from express</h1>");
+  console.log("main middleware");
+  res.send("<h1>hello from main express</h1>");
 });
 
 app.listen(3000);
