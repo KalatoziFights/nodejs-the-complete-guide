811d8b3889559e9d4d87fa31e818acc80f571cfc
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-27 03:58:06 +0400

    sharing data across requests

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // built-in middleware function in Express. It serves static files
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
