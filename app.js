f856d49c3194a519ed23e828c4967c3cebeeaadc
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-29 02:02:26 +0400

    work on handle bars 404 page

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-
-const app = express();
-
-app.set("view engine", "pug");
-app.set("views", "views");
+const expressHbs = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.engine("handlebars", expressHbs());
+app.set("view engine", "handlebars");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // built-in middleware function in Express. It serves static files
