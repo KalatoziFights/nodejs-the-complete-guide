366396b4c1d332c55dea20bf7bd993e59b664f67
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-30 18:25:27 +0400

    adding controllers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
@@ -11,14 +11,12 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-// built-in middleware function in Express. It serves static files
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
   res.status(404).render("404", { pageTitle: "Page Not Found" });
-  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
