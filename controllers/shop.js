70aaf9a6ea082ef206addcdc6496a9bcd040928e
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-05-04 18:36:28 +0400

    deleting cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -65,6 +65,14 @@ exports.postCart = (req, res, next) => {
   res.redirect("/cart");
 };
 
+exports.postCartDeleteProduct = (req, res, next) => {
+  const productId = req.body.productId;
+  Product.findById(productId, (product) => {
+    Cart.deleteProduct(productId, product.price);
+    res.redirect("/");
+  });
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
