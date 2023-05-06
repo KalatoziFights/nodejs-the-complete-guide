16ed97c8eac74f3dad88c66948080a255457e4e2
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-05-07 03:30:11 +0400

    fetch products route

--- app.js
+++ app.js
@@ -4,23 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
 
 const app = express();
+const PORT = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
-db.execute("SELECT * FROM products")
-  .then((result) => {
-    console.log(result[0]);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -30,4 +22,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log(`Server running on port: http://localhost:${PORT}`);
+});
