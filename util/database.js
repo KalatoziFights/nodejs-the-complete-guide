3befed7ab3750ce3fce4cfa65144900412d30ff3
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-05-07 02:20:47 +0400

    retrieving data from mysql

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "node-complete",
-  password: "",
+  password: "giorgi2002",
 });
 
 module.exports = pool.promise();
