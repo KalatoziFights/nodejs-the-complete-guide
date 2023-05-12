ae4c37f4646d1966ac717d328f49d12fa9c5c694
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-05-12 23:32:57 +0400

    create a dummy user

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -29,9 +38,20 @@ Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
+    return User.findByPk(1);
     // console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      User.create({ name: "Giorgi", email: "giorgikalatoz@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000, () => {
       console.log(`Server running on port: http://localhost:${PORT}`);
     });
