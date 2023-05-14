eb603e3de1405a8c5e1ad3161e04b61a8c84d536
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-05-14 22:02:06 +0400

    installing the mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "giorgi2002", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://narutokalatozi:SX7s7N4OhAxRLVH5@mongodb-max.glfebl3.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
