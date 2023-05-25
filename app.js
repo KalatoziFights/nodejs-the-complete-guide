b6eb227c0a306629278cea3de8aaff21d6f80d8c
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-05-26 00:36:30 +0400

    create user

--- app.js
+++ app.js
@@ -53,18 +53,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = User({
-          name: "Giorgi",
-          email: "giorgi@gmail.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -33,4 +33,25 @@ exports.getSignup = (req, res) => {
   });
 };
 
-exports.postSignup = () => {};
+exports.postSignup = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => console.log(err));
+};
