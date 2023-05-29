1e6ed7ea5fedbe53e13a868a02a59a118400d477
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-05-30 03:02:28 +0400

    check field equality

--- routes/auth.js
+++ routes/auth.js
@@ -31,6 +31,12 @@ router.post(
         "Please enter a password with only numbers and text and at least 5 characters."
       )
       .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match!");
+      }
+      return true;
+    }),
   ],
   authController.postSignup
 );
