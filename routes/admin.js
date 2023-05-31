2b7efc2a61454a0ed586e1585248f9d862f28d25
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-06-01 01:02:41 +0400

    validation product editing

--- routes/admin.js
+++ routes/admin.js
@@ -16,7 +16,7 @@ router.get("/products", isAuth, adminController.getProducts);
 router.post(
   "/add-product",
   [
-    body("title").isAlphanumeric().isLength({ min: 3 }).trim(),
+    body("title").isString().isLength({ min: 3 }).trim(),
     body("imageUrl").isURL(),
     body("price").isFloat().trim(),
     body("description").isLength({ min: 5, max: 400 }).trim(),
