7d450d2e8db2604d61da273b04682d44d98eb93b
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-06-03 20:40:00 +0400

    multipart form data

--- routes/admin.js
+++ routes/admin.js
@@ -17,7 +17,7 @@ router.post(
   "/add-product",
   [
     body("title").isString().isLength({ min: 3 }).trim(),
-    body("imageUrl").isURL(),
+    // body("imageUrl").
     body("price").isFloat().trim(),
     body("description").isLength({ min: 5, max: 400 }).trim(),
   ],
