e6e0d43489bf76e01d5fd3af6967d48ac4289bfc
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-06-05 21:19:49 +0400

    set file type headers

--- controllers/shop.js
+++ controllers/shop.js
@@ -166,6 +166,11 @@ exports.getInvoice = (req, res, next) => {
     if (err) {
       return next(err);
     }
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      "inline; filename=`" + invoiceName + "`"
+    );
     res.send(data);
   });
 };
